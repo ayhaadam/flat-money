ffdbdd050c93b9db4cfe0d4d6bee4e20574bcf1d
ayhaadam <ayhaadam@gmail.com>
2013-03-21 23:02:02 +0800

    !

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.fittext.js
+
+window.addEventListener('load', function() {
+    new FastClick(document.body);
+}, false);
+
+$( document ).on(
+"click",
+"a",
+function( event ){
+ 
+// Stop the default behavior of the browser, which
+// is to change the URL of the page.
+event.preventDefault();
+ 
+// Manually change the location of the page to stay in
+// "Standalone" mode and change the URL at the same time.
+location.href = $( event.target ).attr( "href" );
+ 
+}
+);
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Money::Application.config.secret_token = 'e76d454a3a93e7de7c53ed57f18b5882e6ea0e4be440397d20cbb25548ddb20eed63703b01cd4d7c9a9ebc0982c4d24374386e9ac0b66a370ee47eda87381c57'
