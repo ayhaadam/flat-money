4d5f4a116acfb1f113556f76825e4a8778fa7f88
Adam <ayhaadam@gmail.com>
2013-03-25 22:59:36 +0800

    開始實作登入、登出功能及頁面

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,14 +1,19 @@
 class AccountController < ApplicationController
 	before_filter :authenticate_user!
 	def index
-		@cash_accounts = Account.where( :account_type => "cash", :user_id => current_user.id )
-		@cash_sum = Account.sum("balance", :conditions => { :account_type => "cash", :user_id => current_user.id })
-		@credit_card_accounts = Account.where( :account_type => "credit_card", :user_id => current_user.id )
-		@credit_card_sum = Account.sum("balance", :conditions => { :account_type => "credit_card", :user_id => current_user.id })
-		@bank_accounts = Account.where( :account_type => "bank", :user_id => current_user.id )
-		@bank_sum = Account.sum("balance", :conditions => { :account_type => "bank", :user_id => current_user.id })
-		@emoney_accounts = Account.where( :account_type => "emoney", :user_id => current_user.id )
-		@emoney_sum = Account.sum("balance", :conditions => { :account_type => "emoney", :user_id => current_user.id })
+		@all_user_accounts = Account.where(:user_id => current_user.id)
+		@cash_accounts = @all_user_accounts.where( :account_type => "cash" )
+		@cash_sum = @cash_accounts.sum("balance")
+
+		@credit_card_accounts = @all_user_accounts.where( :account_type => "credit_card" )
+		@credit_card_sum = @credit_card_accounts.sum("balance")
+		
+		@bank_accounts = @all_user_accounts.where( :account_type => "bank" )
+		@bank_sum = @bank_accounts.sum("balance")
+		
+		@emoney_accounts = @all_user_accounts.where( :account_type => "emoney" )
+		@emoney_sum = @emoney_accounts.sum("balance")
+
 		@uid = current_user.id
 	end
 	def new
